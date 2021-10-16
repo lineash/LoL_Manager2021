50bcb8ffffc4c2d58bc7b9a20087b1dd3dc0d593
lineash <olol5454@naver.com>
2021-10-16 12:24:46 +0900

    Initial Commit

---
+++ LoL_Manager2021/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LoL_Manager2021
+//
+//  Created by line.ash on 2021/10/16.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
